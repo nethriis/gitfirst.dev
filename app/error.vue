51bc15f0838382d7143cba1d165e4caa22fa6c5e
nethris <david.abou@epitech.eu>
2025-01-20 13:27:51 -0400

    feat: add error page

---
+++ app/error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <NuxtLayout>
+    <h3 class="mx-auto text-zinc-950 dark:text-white font-mono font-semibold">
+      {{ error.statusCode }} | Commit not found.
+    </h3>
+    <p class="mx-auto max-w-[352px] text-sm text-zinc-600 dark:text-zinc-500">
+      {{
+        error.statusCode === 404
+          ? "Looks like this page doesn't exist..."
+          : (error.message ?? 'Unknown error.')
+      }}
+    </p>
+  </NuxtLayout>
+</template>
