806a62794851814eefb09ef29855435c9b86254f
nethris <david.abou@epitech.eu>
2025-01-20 07:54:07 -0400

    fix: application description

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,14 +3,14 @@ const username = ref('')
 </script>
 
 <template>
-  <div class="mx-auto flex flex-col items-center gap-4 max-w-[320px]">
+  <div class="mx-auto flex flex-col items-center gap-4 max-w-[352px]">
     <label for="username" class="text-sm text-zinc-950 dark:text-white">
-      Discover every user's first commit on GitHub.
+      Explore the origins of open-source contributions.
     </label>
     <UserForm
       v-model="username"
       placeholder="nethriis"
-      class="max-w-[320px]"
+      class="max-w-[352px]"
       @submit="() => navigateTo(`/${username.toLowerCase()}`)"
     />
   </div>
